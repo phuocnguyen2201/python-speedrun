74ee27116ab8cafd2fc4c201d887fc141a2b8dfd
phuocnguyen2201 <phuocnguyen2201@gmail.com>
2024-09-29 21:05:31 +0300

    Fix the fomula for future value

--- 27/src/my_code.py
+++ 27/src/my_code.py
@@ -5,16 +5,18 @@ class Stock:
         self.name = name
         self.price = price
         self.quantity = quantity
+        self.total_value = 0
 
     def print_value(self, roi, years):
         value = self.price * self.quantity
-        increase = self.compute_increase(roi, value)
+        increase = self.compute_increase(roi, value, years)
+        self.total_value += increase
         print(f"{self.name} {self.quantity} {self.price}")
-        print(f"Stock value will be {value + increase:.2f}, and profit {increase:.2f}")
+        print(f"Stock value will be {increase:.2f}, and profit {increase - value:.2f}")
 
     @staticmethod
-    def compute_increase(roi, value):
-        return value * roi / 100
+    def compute_increase(roi, value, years):
+        return value * ((1+(roi/100))**years)
 if __name__ == "__main__":
     #Write main program here
     stocks = []
