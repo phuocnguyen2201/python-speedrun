907a123256df72e302476e37e8866a0e067569b5
Phuoc Ng <phuocnguyen2201@gmail.com>
2024-10-07 21:53:46 +0300

    Update for 26

--- 26/src/my_code.py
+++ 26/src/my_code.py
@@ -2,6 +2,7 @@
 #Write class and imports here!
 class Observation:
     def __init__(self, species, number_of_birds, observation_time, position, additional_info):
+
         self.species = species
         self.number_of_birds = number_of_birds
         self.observation_time = observation_time
@@ -11,55 +12,67 @@ class Observation:
     def __str__(self):
         return f"{self.species} {self.number_of_birds} {self.observation_time} {self.position} {self.additional_info}"
 
-    def get_species(self):
+
+    @property
+    def species(self):
         print("getter")
-        return self.species
+        return self._species
 
-    def set_species(self, species):
+    @species.setter
+    def species(self, value):
         print("setter")
-        self.species = species
+        self._species = value
 
-    def get_number_of_birds(self):
+    @property
+    def number_of_birds(self):
         print("getter")
-        return self.number_of_birds
+        return self._number_of_birds
 
-    def set_number_of_birds(self, number_of_birds):
+    @number_of_birds.setter
+    def number_of_birds(self, value):
         print("setter")
-        if number_of_birds < 0:
-            self.number_of_birds = 0
-        else:
-            self.number_of_birds = number_of_birds
+        self._number_of_birds = value if value >= 0 else 0
 
-    def get_observation_time(self):
+
+    @property
+    def observation_time(self):
         print("getter")
-        return self.observation_time
+        return self._observation_time
 
-    def set_observation_time(self, observation_time):
+    @observation_time.setter
+    def observation_time(self, value):
         print("setter")
-        self.observation_time = observation_time
+        self._observation_time = value
 
-    def get_position(self):
+
+    @property
+    def position(self):
         print("getter")
-        return self.position
+        return self._position
 
-    def set_position(self, position):
+    @position.setter
+    def position(self, value):
         print("setter")
-        self.position = position
+        self._position = value
 
-    def get_additional_info(self):
+    @property
+    def additional_info(self):
         print("getter")
-        return self.additional_info
+        return self._additional_info
 
-    def set_additional_info(self, additional_info):
+    @additional_info.setter
+    def additional_info(self, value):
         print("setter")
-        self.additional_info = additional_info
+        self._additional_info = value
 
 if __name__ == "__main__":
     #Write main program below this line
     Observation1 = Observation("crow", 32, "31.01.2020", "Savilahti", "Wind, north 5m/s")
-    print(Observation1)
+
     for i in range(11):
-        Observation1.set_additional_info(Observation1.get_additional_info())
-        Observation1.set_observation_time(Observation1.get_observation_time())
-        Observation1.set_species(Observation1.get_species())
-        Observation1.set_number_of_birds(Observation1.get_number_of_birds())
+        Observation1.additional_info = Observation1.additional_info
+        Observation1.observation_time = Observation1.observation_time
+        Observation1.species = Observation1.species
+        Observation1.number_of_birds = Observation1.number_of_birds
+  
+    print(f'{Observation1.additional_info} {Observation1.number_of_birds} {Observation1.observation_time} {Observation1.position} {Observation1.additional_info}')
